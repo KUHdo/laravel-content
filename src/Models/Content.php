78ddd5f237969e7f4ba48cb94478d8e4a504f6a7
lenrick <lennart.rick@gmx.de>
2023-03-14 16:27:27 +0200

    fixing conflicts2

--- src/Models/Content.php
+++ src/Models/Content.php
@@ -2,12 +2,10 @@
 
 namespace KUHdo\Content\Models;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Query\Builder;
 use KUHdo\Content\Actions\InterpolateTextAction;
 use KUHdo\Content\Database\Factories\ContentFactory;
 use KUHdo\Content\QueryBuilders\ContentQueryBuilder;
@@ -15,7 +13,6 @@ use KUHdo\Content\QueryBuilders\ContentQueryBuilder;
 class Content extends Model
 {
     use HasFactory;
-
 
     public $guarded = [];
 
@@ -34,13 +31,10 @@ class Content extends Model
         return new ContentQueryBuilder($query);
     }
 
-<<<<<<< Updated upstream
-=======
     /**
      *
      * @return BelongsTo
      */
->>>>>>> Stashed changes
     public function translation(): BelongsTo
     {
         return $this->belongsTo(Translation::class);
@@ -56,15 +50,12 @@ class Content extends Model
         return $this->translation()->first()->currentText->value;
     }
 
-<<<<<<< Updated upstream
-=======
     /**
      * This method is called in the HasContent Traits. search and replace style
      *
      * @param array $vars
      * @return string
      */
->>>>>>> Stashed changes
     public function text(array $vars): string
     {
         return (new InterpolateTextAction)($this->translation()->first()->currentText, $vars)->value;
